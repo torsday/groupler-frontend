a90828da2fafd6f0a629aa1bd3a7cb070bc9387c
Steven Miyakawa SAM <samprofessional@gmail.com>
2015-04-18 09:45:48 -0700

    Add boilerplate map stuff

---
+++ main.js
@@ -0,0 +1,5 @@
+L.mapbox.accessToken = 'pk.eyJ1Ijoic2Ftc2Ftc2tpZXMiLCJhIjoiMmVNVm1XRSJ9.UipjupYb5pUYcZxKELWCMg';
+
+var map = L.mapbox.map('map', 'samsamskies.lp1kmn3p');
+
+map.setView([40, -74.50], 9);
